ea4dcbb9318530ec2c60c544a4d1bb5af888db4f
SIDHARTH SINH <160805750+electron244@users.noreply.github.com>
2026-01-06 13:29:20 +0530

    DAY 01
    LeetCode Questions

---
+++ Day_01/LeetCode7.java
@@ -0,0 +1,14 @@
+public class LeetCode7 {
+    public static void main(String[] args) {
+        int x = 1234567;
+        int rev = 0;
+        int sign = x<0?-1:1;
+        int num = Math.abs(x);
+        while(num!=0){
+            int digit = num%10;
+            num = num/10;
+            rev = rev*10 + digit;
+            System.out.println(rev);
+        }
+    }
+}
