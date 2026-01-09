0d3220cd7eb3173d40574d9bd0b28046443d9576
SIDHARTH SINH <160805750+electron244@users.noreply.github.com>
2026-01-09 07:55:57 +0530

    code143

---
+++ Day_04/LeetCode143.java
@@ -0,0 +1,73 @@
+package Day_11;
+
+public class LeetCode143 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        slow.next = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+            first.next = second;
+            second.next = t1;
+
+            first = t1;
+            second = t2;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode143 obj = new LeetCode143();
+        // Create list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5)))));
+
+        obj.reorderList(head);
+
+        // Print reordered list
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        // Output: 1 5 2 4 3
+    }
+}
