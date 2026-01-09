190a94e545e2509c3c92c77a14b66abe1a94a9a7
SIDHARTH SINH <160805750+electron244@users.noreply.github.com>
2026-01-09 07:56:59 +0530

    code946

---
+++ Day_04/LeetCode946.java
@@ -0,0 +1,26 @@
+package Day_11;
+import java.util.Stack;
+
+public class LeetCode946 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushed.length; i++) {
+            st.push(pushed[i]);
+            while (!st.isEmpty() && st.peek() == popped[j] && j < popped.length) {
+                st.pop();
+                j++;
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        LeetCode946 obj = new LeetCode946();
+        int[] pushed = {1,2,3,4,5};
+//        int[] popped = {4,5,3,2,1};
+        int[] popped = {4,3,5,1,2};
+        boolean ans = obj.validateStackSequences(pushed,popped);
+        System.out.println(ans);
+    }
+}
