abfe7fe909f0d6449011ab84ab2cf46e405d8c22
Sidharth Sinh <zalasidharth244@gmail.com>
2026-01-15 21:21:19 +0530

    leet code 125

---
+++ Day_15/LeetCode125.java
@@ -0,0 +1,24 @@
+
+public class LeetCode125 {
+
+    public boolean isPlaindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LeetCode125 obj = new LeetCode125();
+        String s = "A man, a plan, a canal: Panama";
+        boolean ans = obj.isPlaindrome(s);
+        System.out.print(ans);
+    }
+}
