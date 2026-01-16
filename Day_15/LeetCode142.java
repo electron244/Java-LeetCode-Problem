7fa9a2c54af4b5ce66b495def38eed9c12b83684
Sidharth Sinh <zalasidharth244@gmail.com>
2026-01-16 22:53:22 +0530

    LeetCode 142

---
+++ Day_15/LeetCode142.java
@@ -0,0 +1,49 @@
+
+public class LeetCode142 {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        LeetCode142 obj = new LeetCode142();
+        ListNode list = new ListNode(1, new ListNode(2));
+        ListNode ans = obj.detectCycle(list);
+        System.out.print(ans);
+    }
+}
