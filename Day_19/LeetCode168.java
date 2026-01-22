22067e71c5679569a3b97f596933b09d927701f7
Sidharth Sinh <zalasidharth244@gmail.com>
2026-01-22 18:11:52 +0530

    leetCode 168;

---
+++ Day_19/LeetCode168.java
@@ -0,0 +1,17 @@
+public class LeetCode168 {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder res = new StringBuilder();
+        while(columnNumber > 0){
+            columnNumber--;
+            res.insert(0,(char) ((columnNumber%26) + 'A'));
+            columnNumber /= 26;
+        }
+        return res.toString();
+    }
+    public static void main(String[] args) {
+        LeetCode168 obj = new LeetCode168();
+        int num= 28;
+        String ans=obj.convertToTitle(num);
+        System.out.println();
+    }
+}
