a6aede4558c985650a67102b9712b8112d4da8f5
Sidharth Sinh <zalasidharth244@gmail.com>
2026-01-30 17:41:32 +0530

    Leet code 2089

---
+++ Day_23/LeetCode2089.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeetCode2089 {
+
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> idx = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                idx.add(i);
+            }
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        LeetCode2089 obj = new LeetCode2089();
+        int[] nums = { 1, 2, 5, 2, 3 };
+        int target = 2;
+        List<Integer> ans = obj.targetIndices(nums, target);
+        System.out.print(ans);
+    }
+}
