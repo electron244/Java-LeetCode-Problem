1fbbe4b91d628a0df20be78a185696b2c138f459
Sidharth Sinh <zalasidharth244@gmail.com>
2026-02-06 09:05:45 +0530

    Leet code 633

---
+++ Day_25/LeetCode633.java
@@ -0,0 +1,22 @@
+public class LeetCode633 {
+    public boolean judgeSquareSum(int c) {
+        long left = 0;
+        long right = (long) Math.sqrt(c);
+        while (left <= right) {
+            long sum = left * left + right * right;
+            if (sum == c)
+                return true;
+            else if (sum < c)
+                left++;
+            else
+                right--;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LeetCode633 obj = new LeetCode633();
+        boolean ans = obj.judgeSquareSum(3);
+        System.out.println(ans);
+    }
+}
