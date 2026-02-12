50f5255614ab97c34c8817b6c94befabd53ca0c7
Sidharth Sinh <zalasidharth244@gmail.com>
2026-02-12 10:44:40 +0530

    leet code 448

---
+++ Day_28/LeetCode448.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode448{
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i =0;i<nums.length;i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] > 0){
+                nums[index] = -nums[index];
+            }
+        }
+
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] > 0){
+                list.add(i+1);
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        LeetCode448 obj = new LeetCode448();
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List ans = obj.findDisappearedNumbers(nums);
+        System.out.println(ans);
+    }
+}
