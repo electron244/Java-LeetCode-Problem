034c5c0a8bfae2c638770994f30de14599adf5b7
Sidharth Sinh <zalasidharth244@gmail.com>
2026-03-06 20:11:09 +0530

    Leet Code 263

---
+++ Day_35/LeetCode263.java
@@ -0,0 +1,21 @@
+public class LeetCode263 {
+    public boolean isUgly(int n) {
+        if (n <= 0)
+            return false;
+        while (n % 2 == 0)
+            n /= 2;
+        while (n % 3 == 0)
+            n /= 3;
+        while (n % 5 == 0)
+            n /= 5;
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        LeetCode263 obj = new LeetCode263();
+        int n = 6;
+        boolean ans = obj.isUgly(n);
+        System.out.println(ans);
+    }
+}
