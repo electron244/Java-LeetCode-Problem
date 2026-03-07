fe28a0c53fe3b78ddf951b11937ed48411451e71
Sidharth Sinh <zalasidharth244@gmail.com>
2026-03-07 15:49:19 +0530

    Leet Code 680

---
+++ Day_35/LeetCode680.java
@@ -0,0 +1,31 @@
+public class LeetCode680 {
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LeetCode680 obj = new LeetCode680();
+        String s = "abca";
+        boolean ans = obj.validPalindrome(s);
+        System.out.println(ans);
+    }
+}
