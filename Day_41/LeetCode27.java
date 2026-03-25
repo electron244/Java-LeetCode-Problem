c014c1726a1a446d66fb5b45992dd6547866c265
Sidharth Sinh <zalasidharth244@gmail.com>
2026-03-25 23:13:36 +0530

    leet code 27

---
+++ Day_41/LeetCode27.java
@@ -0,0 +1,23 @@
+public class LeetCode27 {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        LeetCode27 obj = new LeetCode27();
+        int ans = obj.removeElement(nums, val);
+        System.out.println(ans);
+
+
+    }
+}
