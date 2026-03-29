e6bac615c5a168512dafe0e5ae891ad39e9443aa
Sidharth Sinh <zalasidharth244@gmail.com>
2026-03-29 22:55:26 +0530

    Leet Code 100

---
+++ Day_43/LeetCode100.java
@@ -0,0 +1,40 @@
+public class LeetCode100 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        LeetCode100 obj = new LeetCode100();
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+        boolean ans = obj.isSameTree(p, q);
+        System.out.println(ans);
+    }
+}
