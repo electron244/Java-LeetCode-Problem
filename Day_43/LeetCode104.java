a2d1e5798c3ded738e1b91c55e35de308133f28f
Sidharth Sinh <zalasidharth244@gmail.com>
2026-03-30 18:56:10 +0530

    Leet Code 104

---
+++ Day_43/LeetCode104.java
@@ -0,0 +1,36 @@
+public class LeetCode104 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        LeetCode104 obj = new LeetCode104();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        int ans = obj.maxDepth(root);
+        System.out.println(ans);
+    }
+}
