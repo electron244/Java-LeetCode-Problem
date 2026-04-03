b364215ebe5cfa6c50a68ecedaa76ba08df194b4
Sidharth Sinh <zalasidharth244@gmail.com>
2026-04-03 23:02:14 +0530

    leet code 222

---
+++ Day_45/LeetCode222.java
@@ -0,0 +1,36 @@
+public class LeetCode222 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left+right+1;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        int ans = countNodes(root);
+        System.out.println(ans);
+    }
+}
