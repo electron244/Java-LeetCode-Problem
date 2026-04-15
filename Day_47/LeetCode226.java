25af74e3380e253af0a47db5da7ddc99879ba844
Sidharth Sinh <zalasidharth244@gmail.com>
2026-04-15 23:37:21 +0530

    leet code 226

---
+++ Day_47/LeetCode226.java
@@ -0,0 +1,43 @@
+
+public class LeetCode226 {
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1); 
+        root.right = new TreeNode(3); 
+        invertTree(root);
+    }
+}
